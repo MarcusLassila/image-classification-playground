d9920f9473d0e3f07d246ffe664da4435ab63b44
Marcus Lassila <marcus@ducce.net>
2023-06-24 16:07:28 +0200

    Flowers102 data setup

---
+++ src/data_setup.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import random
+import torchvision
+
+class Flowers102DataSetup:
+    
+    def __init__(self, transform=None):
+        self.train_dataset = torchvision.datasets.Flowers102(root='./data', split='train', transform=transform, download=True)
+        self.valid_dataset = torchvision.datasets.Flowers102(root='./data', split='val', transform=transform, download=True)
+        self.test_dataset  = torchvision.datasets.Flowers102(root='./data', split='test', transform=transform, download=True)
+
+    def plot_random_samples(self, dim=4):
+        fig = plt.figure(figsize=(10, 10))
+        for i in range(1, dim ** 2 + 1):
+            image, label = random.choice(self.train_dataset)
+            fig.add_subplot(dim, dim, i)
+            plt.imshow(image)
+            plt.title(label)
+            plt.axis("off")
+        plt.show()
+
+if __name__ == '__main__':
+    flowers102 = Flowers102DataSetup()
+    flowers102.plot_random_samples()
