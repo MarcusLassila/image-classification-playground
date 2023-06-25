a62c92e22ae35817aa5fdfa790a88a3e4d1af4bd
Marcus Lassila <marcus@ducce.net>
2023-06-25 13:08:21 +0200

    use all data

--- src/data_setup.py
+++ src/data_setup.py
@@ -4,8 +4,8 @@ import torchvision
 class Flowers102DataSetup:
 
     def __init__(self, train_transform=None, test_transform=None, batch_size=1, num_workers=0):
-        self.train_dataset = Subset(torchvision.datasets.Flowers102(root='./data', split='train', transform=train_transform, download=True), range(400))
-        self.valid_dataset = Subset(torchvision.datasets.Flowers102(root='./data', split='val', transform=None, download=True), range(100))
+        self.train_dataset = torchvision.datasets.Flowers102(root='./data', split='train', transform=train_transform, download=True)
+        self.valid_dataset = torchvision.datasets.Flowers102(root='./data', split='val', transform=None, download=True)
         self.test_dataset  = torchvision.datasets.Flowers102(root='./data', split='test', transform=test_transform, download=True)
 
         self.train_dataloader = DataLoader(self.train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
